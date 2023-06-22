3f0ca0f35660a5cdbdb8133b2bc12d4b4bede0f7
Rudy Ayitinya <contact@ayitinya.me>
2023-06-22 07:09:46 +0100

    moves tts to viewmodel

--- app/src/main/java/com/ayitinya/englishdictionary/di/TextToSpeechModule.kt
+++
@@ -1,4 +0,0 @@
-package com.ayitinya.englishdictionary.di
-
-object TextToSpeechModule {
-}
--- app/src/main/java/com/ayitinya/englishdictionary/ui/definition/TextToSpeechInitState.kt
+++ app/src/main/java/com/ayitinya/englishdictionary/ui/definition/TextToSpeechInitState.kt
@@ -1,4 +1,5 @@
 package com.ayitinya.englishdictionary.ui.definition
 
 enum class TextToSpeechInitState {
+    SUCCESS, FAILED, INITIALISING, READY
 }
