350ac919c8cc915f6981ef656f4ddc59d5f0747b
Rudy Ayitinya Sulley <78674026+ayitinya@users.noreply.github.com>
2024-04-28 16:26:43 +0000

    replaces sqliteZstd with a git submodule

--- sqliteZstd/python/lib/datasette.py
+++
@@ -1,9 +0,0 @@
-from datasette import hookimpl
-from . import load
-
-
-@hookimpl
-def prepare_connection(conn):
-    conn.enable_load_extension(True)
-    load(conn)
-    conn.enable_load_extension(False)
